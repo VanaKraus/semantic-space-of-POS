1cad089e3ad83e91134f98f93896a2a3a98f6271
Arnold Stanovský <stanoarn@fit.cvut.cz>
2024-11-25 21:39:32 +0100

    fix morphodita more

--- morphodita.py
+++ morphodita.py
@@ -5,35 +5,35 @@ import sys
 import os
 
 
-def morphodita(filenames, model_name= "czech-morfflex2.0-pdtc1.0-220710"):
-    for filename in filenames:
-        with open(filename, "r") as fs:
-            # this way tokens are separated by \n\n (double new line)
-            # morphodita reads this as if each token was its own sentence
-            # meaning the tagging hopefully shouldn't be contextual
-            text = "\n".join(fs.readlines())
+def morphodita(filename, model_name= "czech-morfflex2.0-pdtc1.0-220710"):
+    with open(filename, "r") as fs:
+        # this way tokens are separated by \n\n (double new line)
+        # morphodita reads this as if each token was its own sentence
+        # meaning the tagging hopefully shouldn't be contextual
+        text = "\n".join(fs.readlines())
 
-        query = {
-            "data": text,
-            "model": model_name,
-            # "model": "english-morphium-wsj-140407",
-            "guesser": "no",
-            "input": "vertical",
-            "output": "vertical",
-        }
+    query = {
+        "data": text,
+        "model": model_name,
+        # "model": "english-morphium-wsj-140407",
+        "guesser": "no",
+        "input": "vertical",
+        "output": "vertical",
+    }
 
-        response = requests.post(
-            "https://lindat.mff.cuni.cz/services/morphodita/api/tag", data=query
+    response = requests.post(
+        "https://lindat.mff.cuni.cz/services/morphodita/api/tag", data=query
+    )
+
+    with open(os.path.join("annotated", filename), "w+") as fs:
+        fs.write(
+            response.json()["result"]
+            .replace('"', "")
+            .replace("'", "")
+            .replace("\n\n", "\n")
         )
-
-        with open(os.path.join("annotated", filename), "w+") as fs:
-            fs.write(
-                response.json()["result"]
-                .replace('"', "")
-                .replace("'", "")
-                .replace("\n\n", "\n")
-            )
 
 
 if __name__ == "__main__":
-    morphodita(sys.argv[1:])
+    for arg in sys.argv[1:]:
+        morphodita(arg)
