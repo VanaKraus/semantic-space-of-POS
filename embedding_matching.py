97cdb21f3da6c28d15cb3bfcb403c501aaad46e1
Ivan Kraus <ivakra@centrum.cz>
2025-02-03 00:43:39 +0100

    Add embedding matching

---
+++ embedding_matching.py
@@ -0,0 +1,53 @@
+import pandas as pd
+
+DIMENSIONS = 100
+ENCODING = "latin-1"  # UTF-8 doesn't work for Czech embeddings for some reason
+
+
+def match_embeddings(
+    embeddings_path, words_path, out_tsv, target_size: int, encoding=ENCODING
+):
+    def log_progress(line, filled):
+        print(f"{line} lines w/ {filled} embeddings filled")
+
+    df = pd.read_csv(words_path, sep="\t", encoding=encoding)
+    word_list = list(df["Word"])
+    word_set = set(word_list)
+
+    dimensions_selector = [f"D{i+1}" for i in range(DIMENSIONS)]
+
+    with open(embeddings_path, "r", encoding=encoding) as f:
+        ctr_lines, ctr_df = 0, 0
+        for line in f:
+            ctr_lines += 1
+            if ctr_lines % 10000 == 0:
+                log_progress(ctr_lines, ctr_df)
+
+            linespl = line.strip().split(" ")
+
+            if len(linespl) != DIMENSIONS + 1:
+                continue
+
+            word = linespl[0]
+            if word not in word_set:
+                continue
+
+            windex = word_list.index(word)
+            df.loc[windex, dimensions_selector] = linespl[1:]
+
+            ctr_df += 1
+            if ctr_df % 50 == 0:
+                log_progress(ctr_lines, ctr_df)
+
+    notna_selector = df.notna().all(axis=1)
+    print(f"{int(notna_selector.sum())} valid words matched ({target_size=})")
+
+    df = (
+        df.loc[notna_selector]
+        .reset_index(drop=True)
+        .sort_values(by=["Freq_Sum"], ascending=False)[:target_size]
+    )
+
+    df.to_csv(out_tsv, sep="\t", encoding=ENCODING)
+
+    return df.astype({s: "float" for s in dimensions_selector})
--- prepare.py
+++ prepare.py
@@ -1,35 +1,10 @@
 #!/usr/bin/env python3
 
-import sys
 
 import pandas as pd
-import re
 
 
-pos_legend = {
-    "N": ("substantivum", "Noun"),
-    "A": ("adjektivum", "Adjective"),
-    "P": ("zájmeno", "Pronoun"),
-    "C": ("číslovka", "Numeral"),
-    "V": ("sloveso", "Verb"),
-    "D": ("adverbium", "Adverb"),
-    "R": ("předložka", "Preposition"),
-    "J": ("spojka", "Conjunction"),
-    "T": ("částice", "Particle"),
-    "I": ("citoslovce", "Interjection"),
-}
-
-
-def get_pos_info(tag):
-    pos_code = tag[0] if tag else None
-    pos_czech, pos_english = pos_legend.get(pos_code, (None, None))
-    return pos_code, pos_czech, pos_english
-
-
-# MAX_ROWS  Maximum number of words to keep, the rest will be truncated
-
-
-def prepare_vectors(input_file, output_file, MAX_ROWS=1000):
+def prepare_vectors(input_file, output_file):
     data = []
 
     # Load the dataset and remove the first line
@@ -41,9 +16,6 @@ def prepare_vectors(input_file, output_file, MAX_ROWS=1000):
                 print(linesplt)
             else:
                 data += [linesplt]
-
-    # Limit the number of rows to MAX_ROWS + some overhead for interpunction that will be filtered out later
-    data = data[: int(MAX_ROWS * 1.5)]
 
     # Determine the number of columns dynamically based on the first data row
     num_vector_columns = len(data[0]) - 1
@@ -58,19 +30,19 @@ def prepare_vectors(input_file, output_file, MAX_ROWS=1000):
     print(f"Processing complete. The output has been saved as '{output_file}'.")
 
 
-if __name__ == "__main__":
-    args = sys.argv[1:]
+# if __name__ == "__main__":
+#     args = sys.argv[1:]
 
-    if (ln := len(args)) != 3:
-        print(f"incorrect no. of arguments: {ln} found", file=sys.stderr)
-        print(f"3 arguments expected:", file=sys.stderr)
-        print(f"\t1. original vectors", file=sys.stderr)
-        print(f"\t2. output file path", file=sys.stderr)
-        print(f"\t3. max no. of rows", file=sys.stderr)
+#     if (ln := len(args)) != 3:
+#         print(f"incorrect no. of arguments: {ln} found", file=sys.stderr)
+#         print(f"3 arguments expected:", file=sys.stderr)
+#         print(f"\t1. original vectors", file=sys.stderr)
+#         print(f"\t2. output file path", file=sys.stderr)
+#         print(f"\t3. max no. of rows", file=sys.stderr)
 
-        sys.exit(1)
+#         sys.exit(1)
 
-    input_file, output_file, max_rows = args
-    max_rows = int(max_rows)
+#     input_file, output_file, max_rows = args
+#     max_rows = int(max_rows)
 
-    prepare_vectors(input_file, output_file, max_rows)
+#     prepare_vectors(input_file, output_file, max_rows)
